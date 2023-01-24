8d964eb82a0c2f5e14573874acbaca4a7c1eff93
cristhianpoveda <cristhianpoveda12@gmail.com>
2023-01-24 09:25:58 -0500

    vel limit

--- packages/navigation/src/forward_kinematics_node.py
+++ packages/navigation/src/forward_kinematics_node.py
@@ -3,7 +3,7 @@ import os
 import rospy
 import math
 from duckietown.dtros import DTROS, NodeType
-from duckietown_msgs.msg import WheelEncoderStamped
+from duckietown_msgs.msg import WheelEncoderStamped, Twist2DStamped
 from geometry_msgs.msg import TwistWithCovarianceStamped
 
 class ForwardKinematics(DTROS):
@@ -11,10 +11,10 @@ class ForwardKinematics(DTROS):
         # initialize the DTROS parent class
         super(ForwardKinematics, self).__init__(node_name=node_name, node_type=NodeType.LOCALIZATION)
 
-        self.r = 0.033
-        self.l = 0.0484
-        self.encoder_resolution = 135
-        self.publish_period = 0.1111
+        self.r = rospy.get_param('~r')
+        self.l = rospy.get_param('~l')
+        self.encoder_resolution = rospy.get_param('~encoder_resolution')
+        self.publish_period = rospy.get_param('~publish_period')
         self.right_tick = 0
         self.left_tick = 0
         self.last_right_tick = 0
@@ -27,9 +27,7 @@ class ForwardKinematics(DTROS):
         self.sub_left_encoder = rospy.Subscriber('/duckiebot4/left_wheel_encoder_node/tick', WheelEncoderStamped, self.cb_left_wheel)
 
         # publishers
-        self.pub_rigth_phi = rospy.Publisher('~right_phi/twist', TwistWithCovarianceStamped, queue_size=2)
-        self.pub_left_phi = rospy.Publisher('~left_phi/twist', TwistWithCovarianceStamped, queue_size=2)
-        self.pub_vel = rospy.Publisher('~duckiebot_base/twist', TwistWithCovarianceStamped, queue_size=2)
+        self.pub_vel = rospy.Publisher('~velocity', Twist2DStamped, queue_size=2)
 
         rospy.Timer(rospy.Duration(self.publish_period), self.cb_timer)
 
@@ -46,48 +44,10 @@ class ForwardKinematics(DTROS):
         phi_dot_right = (self.right_tick - self.last_right_tick) * 2 * math.pi / (self.encoder_resolution * self.publish_period)
         phi_dot_left = (self.left_tick - self.last_left_tick) * 2 * math.pi / (self.encoder_resolution * self.publish_period)
 
-        if self.pub_rigth_phi.anybody_listening():
-
-            right_twist = TwistWithCovarianceStamped()
-            right_twist.header.stamp = rospy.get_rostime()
-            right_twist.header.frame_id = "duckiebot4/right_wheel_axis"
-            right_twist.twist.twist.linear.x = phi_dot_right * self.r
-            right_twist.twist.twist.linear.y = 0
-            right_twist.twist.covariance = [2e-08, 0, 0, 0, 0, 0,     # x
-                                            0, 2e-08, 0, 0, 0, 0,     # y
-                                            0, 0, 2e-08, 0, 0, 0,     # z
-                                            0, 0, 0, 2e-08, 0, 0,     # roll
-                                            0, 0, 0, 0, 2e-08, 0,     # pitch
-                                            0, 0, 0, 0, 0, 2e-08]    # yaw
-            self.pub_rigth_phi.publish(right_twist)
-
-        if self.pub_left_phi.anybody_listening():
-
-            left_twist = TwistWithCovarianceStamped()
-            left_twist.header.stamp = rospy.get_rostime()
-            left_twist.header.frame_id = "duckiebot4/left_wheel_axis"
-            left_twist.twist.twist.linear.x = phi_dot_left * self.r
-            right_twist.twist.twist.linear.y = 0
-            left_twist.twist.covariance = [2e-08, 0, 0, 0, 0, 0,     # x
-                                            0, 2e-08, 0, 0, 0, 0,     # y
-                                            0, 0, 2e-08, 0, 0, 0,     # z
-                                            0, 0, 0, 2e-08, 0, 0,     # roll
-                                            0, 0, 0, 0, 2e-08, 0,     # pitch
-                                            0, 0, 0, 0, 0, 2e-08]    # yaw
-            self.pub_left_phi.publish(left_twist)
-
-        twist_msg = TwistWithCovarianceStamped()
+        twist_msg = Twist2DStamped()
         twist_msg.header.stamp = rospy.get_rostime()
-        twist_msg.header.frame_id = "duckiebot4/footprint"
-        twist_msg.twist.twist.linear.x = (self.r / 2) * (phi_dot_right + phi_dot_left)
-        twist_msg.twist.twist.linear.y = 0
-        twist_msg.twist.twist.angular.z = self.r * (phi_dot_right - phi_dot_left) / (2 * self.l)
-        twist_msg.twist.covariance = [2e-08, 0, 0, 0, 0, 0,     # x
-                                      0, 2e-08, 0, 0, 0, 0,     # y
-                                      0, 0, 2e-08, 0, 0, 0,     # z
-                                      0, 0, 0, 2e-08, 0, 0,     # roll
-                                      0, 0, 0, 0, 2e-08, 0,     # pitch
-                                      0, 0, 0, 0, 0, 2e-08]    # yaw
+        twist_msg.v = (self.r / 2) * (phi_dot_right + phi_dot_left)
+        twist_msg.omega = self.r * (phi_dot_right - phi_dot_left) / (2 * self.l)
 
         self.pub_vel.publish(twist_msg)
 
