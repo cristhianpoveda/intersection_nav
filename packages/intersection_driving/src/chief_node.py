921496e7ac0109dd36caa3f22707ed7a49e4e006
cristhianpoveda <cristhianpoveda12@gmail.com>
2023-05-17 17:36:46 -0500

    action process

--- packages/intersection_driving/src/chief_node.py
+++ packages/intersection_driving/src/chief_node.py
@@ -9,8 +9,8 @@ from intersection_msgs.srv import DetectStopSign, DetectStopSignResponse, MakeDe
 from duckietown_msgs.srv import SetValue, SetValueRequest
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseResult, MoveBaseFeedback
 from duckietown_msgs.msg import FSMState
-from std_msgs.msg import Float32
 from std_srvs.srv import SetBool, SetBoolRequest, SetBoolResponse
+from geometry_msgs.msg import Pose
 
 class ChiefNode(DTROS):
     # create messages that are used to publish feedback/result
@@ -26,7 +26,6 @@ class ChiefNode(DTROS):
         self._way3 = rospy.get_param('~way3')
         
         self.pub_state = rospy.Publisher('/duckiebot4/fsm_node/mode', FSMState, queue_size=1)
-        self.pub_time = rospy.Publisher('~debug/calculation_time', Float32, queue_size=0)
 
         self._action_name = node_name
         self._as = actionlib.SimpleActionServer(self._action_name, intersection_msgs.msg.IntersectionDrivingAction, execute_cb=self.execute_cb, auto_start = False)
@@ -58,53 +57,52 @@ class ChiefNode(DTROS):
         self._as.set_succeeded(self._result)
 
     def execute_cb(self, goal):
-        # helper variables
+
         success = False
         
-        # start information
+        # starting message
         rospy.loginfo('%s: Executing action: Navigate to destination: %s .' % (self._action_name, goal.destination.data))
 
-        # # SWITCH STOP SIGN DETECTION ON
+        # SWITCH STOP SIGN DETECTION ON
 
-        # switch_response = self.call_swicth_srv('/duckiebot4/stop_sign_detector_node/switch', True)
+        switch_response = self.call_swicth_srv('/duckiebot4/stop_sign_detector_node/switch', True)
 
-        # if not switch_response.success:
-        #     self.fail_msg()
-        #     return
+        if not switch_response.success:
+            self.fail_msg()
+            return
 
-        # # ARRIVE AT STOP SIGN
+        # ARRIVE AT STOP SIGN
 
-        # rospy.wait_for_service('/duckiebot4/stop_sign_detector_node/detect_stop_sign')
-        # try:
-        #     arr = rospy.ServiceProxy('/duckiebot4/stop_sign_detector_node/detect_stop_sign', DetectStopSign)
-        #     arrived_response = arr()
+        rospy.wait_for_service('/duckiebot4/stop_sign_detector_node/detect_stop_sign')
+        try:
+            arrived_response = Pose()
+            arr = rospy.ServiceProxy('/duckiebot4/stop_sign_detector_node/detect_stop_sign', DetectStopSign)
+            arrived_response = arr()
 
-        #     # 1st task feedback
-        #     self._feedback.tasks = "Arrived at intersection"
-        #     self._as.publish_feedback(self._feedback)
+            # 1st task feedback
+            self._feedback.tasks = "Arrived at intersection"
+            self._as.publish_feedback(self._feedback)
 
-        # except rospy.ServiceException as e:
-        #     rospy.loginfo("Arrive at stop sign service call failed: %s"%e)
-        #     self.fail_msg()
-        #     return
-
-        start = rospy.get_rostime()
+        except rospy.ServiceException as e:
+            rospy.loginfo("Arrive at stop sign service call failed: %s"%e)
+            self.fail_msg()
+            return
 
         # SWITCH STOP SIGN DETECTION OFF
 
-        # switch_response = self.call_swicth_srv('/duckiebot4/stop_sign_detector_node/switch', False)
+        switch_response = self.call_swicth_srv('/duckiebot4/stop_sign_detector_node/switch', False)
 
-        # if not switch_response.success:
-        #     self.fail_msg()
-        #     return
+        if not switch_response.success:
+            self.fail_msg()
+            return
 
         # SWITCH OBJECT DETECTION ON
 
-        # switch_response = self.call_swicth_srv('/duckiebot4/runtime_detector/switch', True)
+        switch_response = self.call_swicth_srv('/duckiebot4/runtime_detector/switch', True)
 
-        # if not switch_response.success:
-        #     self.fail_msg()
-        #     return
+        if not switch_response.success:
+            self.fail_msg()
+            return
 
         # UPDATE DUCKIEBOT POSE ON MAP
 
@@ -124,7 +122,7 @@ class ChiefNode(DTROS):
             self.fail_msg()
             return
 
-        time.sleep(0.1)
+        time.sleep(0.13)
 
         # MAKE DECISION
         decision = 1
@@ -150,19 +148,19 @@ class ChiefNode(DTROS):
 
         # SWITCH OBJECT DETECTION OFF
 
-        # switch_response = self.call_swicth_srv('/duckiebot4/runtime_detector/switch', False)
+        switch_response = self.call_swicth_srv('/duckiebot4/runtime_detector/switch', False)
 
-        # if not switch_response.success:
-        #     self.fail_msg()
-        #     return
+        if not switch_response.success:
+            self.fail_msg()
+            return
 
-        # # SWITCH LOCALIZATION ON
+        # SWITCH LOCALIZATION ON
 
-        # switch_response = self.call_swicth_srv('/duckiebot4/forward_kinematics_node/switch', True)
+        switch_response = self.call_swicth_srv('/duckiebot4/forward_kinematics_node/switch', True)
 
-        # if not switch_response.success:
-        #     self.fail_msg()
-        #     return
+        if not switch_response.success:
+            self.fail_msg()
+            return
 
         # CHANGE FINITE STATE MACHINE TO INTERSECTION CONTROL
 
@@ -170,78 +168,71 @@ class ChiefNode(DTROS):
         state_nav.state = "INTERSECTION_CONTROL"
         self.pub_state.publish(state_nav)
 
-        end = rospy.get_rostime()
-        time_t = (end - start).to_sec()
-
-        if self.pub_time.anybody_listening():
-
-            self.pub_time.publish(time_t)
-
         success = True
 
-        # # NAVIGATE
+        # NAVIGATE
 
-        # if decision == 1:
+        if decision == 1:
 
-        #     # Decision feedback
-        #     self._feedback.tasks = "Action: wait"
-        #     self._as.publish_feedback(self._feedback)
-        #     success = True
+            # Decision feedback
+            self._feedback.tasks = "Action: wait"
+            self._as.publish_feedback(self._feedback)
+            success = True
 
-        # else:
-        #     # Decision feedback
-        #     self._feedback.tasks = "Aciton: cross"
-        #     self._as.publish_feedback(self._feedback)
+        else:
+            # Decision feedback
+            self._feedback.tasks = "Aciton: cross"
+            self._as.publish_feedback(self._feedback)
 
-        #     # SET NAVIGATION GOAL
+            # SET NAVIGATION GOAL
 
-        #     if goal.destination.data == "1":
-        #         x = self._way1.x
-        #         y = self._way1.r
-        #         rz = self._way1.rz
-        #         rw = self._way1.rw
+            if goal.destination.data == "1":
+                x = self._way1.x
+                y = self._way1.r
+                rz = self._way1.rz
+                rw = self._way1.rw
 
-        #     elif goal.destination.data == "2":
-        #         x = self._way2.x
-        #         y = self._way2.r
-        #         rz = self._way2.rz
-        #         rw = self._way2.rw
+            elif goal.destination.data == "2":
+                x = self._way2.x
+                y = self._way2.r
+                rz = self._way2.rz
+                rw = self._way2.rw
 
-        #     elif goal.destination.data == "3":
-        #         x = self._way3.x
-        #         y = self._way3.r
-        #         rz = self._way3.rz
-        #         rw = self._way3.rw
+            elif goal.destination.data == "3":
+                x = self._way3.x
+                y = self._way3.r
+                rz = self._way3.rz
+                rw = self._way3.rw
 
-        #     nav_goal = MoveBaseGoal()
-        #     nav_goal.target_pose.header.frame_id = 'map'
-        #     nav_goal.target_pose.pose.position.x = x
-        #     nav_goal.target_pose.pose.position.y = y
-        #     nav_goal.target_pose.pose.position.z = 0.0
-        #     nav_goal.target_pose.pose.orientation.x = 0.0
-        #     nav_goal.target_pose.pose.orientation.y = 0.0
-        #     nav_goal.target_pose.pose.orientation.z = rz
-        #     nav_goal.target_pose.pose.orientation.w = rw
+            nav_goal = MoveBaseGoal()
+            nav_goal.target_pose.header.frame_id = 'map'
+            nav_goal.target_pose.pose.position.x = x
+            nav_goal.target_pose.pose.position.y = y
+            nav_goal.target_pose.pose.position.z = 0.0
+            nav_goal.target_pose.pose.orientation.x = 0.0
+            nav_goal.target_pose.pose.orientation.y = 0.0
+            nav_goal.target_pose.pose.orientation.z = rz
+            nav_goal.target_pose.pose.orientation.w = rw
 
-        #     client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
+            client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
 
-        #     client.wait_for_server()
+            client.wait_for_server()
 
-        #     client.send_goal(nav_goal, feedback_cb = self.feedback_callback)
+            client.send_goal(nav_goal, feedback_cb = self.feedback_callback)
 
-        #     client.wait_for_result()
+            client.wait_for_result()
 
-        #     rospy.loginfo('[Result] State: %d'% (client.get_state()))
+            rospy.loginfo('[Result] State: %d'% (client.get_state()))
 
-        #     success = True
+            success = True
 
         # SWITCH LOCALIZATION OFF
 
-        # switch_response = self.call_swicth_srv('/duckiebot4/forward_kinematics_node/switch', False)
+        switch_response = self.call_swicth_srv('/duckiebot4/forward_kinematics_node/switch', False)
 
-        # if not switch_response.success:
-        #     self.fail_msg()
-        #     return
+        if not switch_response.success:
+            self.fail_msg()
+            return
 
         state_end = FSMState()
         state_end.state = "NORMAL_JOYSTICK_CONTROL"
